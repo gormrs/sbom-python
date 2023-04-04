8564e7ad52a9e299ca2871a40f7b0a837152f6c9
gormrs <gormrs@uio.no>
2023-04-04 19:11:25 +0200

    Update to f-strings

--- sbom.py
+++ sbom.py
@@ -3,13 +3,14 @@ from traverse import *
 
 def main(argv):
     if (len(argv) != 2):
-        print("Error, usage is: sbom.py [dir]")
+        print(f"Error, usage is: {argv[0]} [dir]")
         return 1
     
     directory = argv[1]
-    print("Searching for repos in directory", directory)
+    print(f"Searching for repos in directory {directory}")
 
-    print("Found", findRepoTraverse(directory), "repos", "in", directory)
+    repo_count = findRepoTraverse(directory)
+    print(f"Found {repo_count} repos in {directory}")
 
 if __name__ == "__main__":
     main(sys.argv)
