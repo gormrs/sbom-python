6842cb1d40f3ffdedf00bd4e0c5da426d3586715
gormrs <gormrs@uio.no>
2023-04-04 19:50:14 +0200

    added guard clausese for the traverse

--- traverse.py
+++ traverse.py
@@ -3,6 +3,21 @@ import os
 def findRepoTraverse(directory):
     repoCount = 0
 
+    if not directory:
+        print("Error, no directory specified")
+        return 0
+    
+    if type(directory) != str:
+        print("Error, directory must be a string")
+        return 0 
+
+    # checks if the directory is valid
+    if not os.path.isdir(directory):
+        print(f"Error, {directory} is not a valid directory")
+        return 0
+    
+    
+
 
     for root, dirs, files in os.walk(directory):
         if ".git" in dirs:
