e78f5dde532824bcf388ae53067e6b206aa32ea2
gormrs <gormrs@uio.no>
2023-04-05 17:38:01 +0200

    refactoring: more error handling and exit codes

--- sbom.py
+++ sbom.py
@@ -5,7 +5,7 @@ from file_util import *
 def main(argv):
     if (len(argv) != 2):
         print(f"Error, usage is: {argv[0]} [dir]")
-        return 
+        sys.exit(2)
     
     directory = argv[1]
     print(f"Searching for repos in directory {directory}")
--- traverse.py
+++ traverse.py
@@ -1,28 +1,38 @@
 import json
 import os
 from file_util import *
+import sys
 
 def find_repo_traverse(directory):
     repo_count = 0
+    found_a_sbomfile = False
 
     if not directory:
         print("Error, no directory specified")
-        return 0
+        sys.exit(1)
     
     if type(directory) != str:
         print("Error, directory must be a string")
-        return 0 
+        sys.exit(1)
+
+    if not os.path.isdir(directory):
+        print("Error, directory does not exist")
+        sys.exit(1)
 
     for root, dirs, files in os.walk(directory):
         if ".git" in dirs:
             repo_count += 1
             if "requirements.txt" in os.listdir(root):
                 get_python_requirements(root)
+                found_a_sbomfile = True
             if "package.json" in os.listdir(root):
                 get_java_dependencies(root)
+                found_a_sbomfile = True
             dirs.remove(".git")
 
-
+    if not found_a_sbomfile:
+        print("Error, no sbom file found")
+        sys.exit(1)
     return repo_count
 
 def get_python_requirements(directory):
